ac7fab214da44f27d9b61450d0487d598cca80ca
Tuomas <tuomas123lahtinen@gmail.com>
2023-11-24 08:57:20 +0200

    Setup solution and project

---
+++ Globomantics.UI/Globomantics.UI.WPF/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Globomantics.UI.WPF")]
+[assembly: AssemblyDescription("A library of reusable styles, themes, icons, controls, and other stuff")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Globomantics")]
+[assembly: AssemblyProduct("Globomantics.UI.WPF")]
+[assembly: AssemblyCopyright("Copyright ©  2023 Globomantics")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
