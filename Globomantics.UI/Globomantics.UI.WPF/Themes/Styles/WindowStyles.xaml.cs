8a73f7fb5ebd21b4a51df2929ecf7e80c327d956
Tuomas <tuomas123lahtinen@gmail.com>
2024-09-14 07:15:30 +0300

    Drop MainWindowModel in favor of code behind
    Found it example worthy to implement code behind for ResourceDictionary.
    Also the mvvm felt a bit heavy and dropped in favor of the simplicity and lack of dependencies.

---
+++ Globomantics.UI/Globomantics.UI.WPF/Themes/Styles/WindowStyles.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace Globomantics.UI.WPF.Themes.Styles
+{
+    /// <summary>
+    /// Implements MainWindow template's button handlers
+    /// </summary>
+    public partial class WindowStyles : ResourceDictionary
+    {
+        public WindowStyles()
+        {
+            InitializeComponent();
+        }
+
+        Window GetWindow(object sender) => (sender as FrameworkElement)?.TemplatedParent as Window;
+
+        private void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            GetWindow(sender).WindowState = WindowState.Minimized;
+        }
+
+        private void CloseClick(object sender, RoutedEventArgs e)
+        {
+            GetWindow(sender).Close();
+        }
+
+        private void MaximizeRestoreClick(object sender, RoutedEventArgs e)
+        {
+            var window = GetWindow(sender);
+            window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void ThemeSwitchClick(object sender, RoutedEventArgs e)
+        {
+            var theme = Theme.ThemeType != ThemeType.Light ? ThemeType.Light : ThemeType.Dark;
+            Theme.LoadThemeType(theme);
+        }
+    }
+}
