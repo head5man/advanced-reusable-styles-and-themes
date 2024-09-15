649d0a68cb3792498c757144543bd8e2d5d27669
Tuomas <tuomas123lahtinen@gmail.com>
2024-09-15 18:50:17 +0300

    Add converter for XamlImage to ImageSource

---
+++ Globomantics.UI/Globomantics.UI.WPF/Converters/XamlImageKeyToImageSourceConverter.cs
@@ -0,0 +1,59 @@
+﻿using Globomantics.UI.WPF.Controls;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Globomantics.UI.WPF.Converters
+{
+    public class XamlImageKeyToImageSourceConverter : MarkupExtension, IValueConverter
+    {
+        public double Height { get; set; } = 16;
+        public double Width { get; set; } = 16;
+        public string ForegroundResourceKey { get; set; } = "WindowHeaderForeground";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var keyParsed = Enum.TryParse(ForegroundResourceKey, out ThemeResourceKey foregroundKey);
+            if ((value is string key) is false || !keyParsed)
+            { 
+                return null;
+            }
+
+            var element = new XamlImage
+            {
+                Template = (ControlTemplate)Application.Current.TryFindResource(key),
+                Foreground = (Brush)Theme.GetResource(foregroundKey),
+                Height = Height,
+                Width = Width
+            };
+            var size = new Size((int)element.Width, (int)element.Height);
+            element.Measure(size);
+            element.Arrange(new Rect(size));
+
+            var dpiScale = VisualTreeHelper.GetDpi(element);
+
+            var rtb = new RenderTargetBitmap(
+                (int)element.ActualWidth,
+                (int)element.ActualHeight,
+                dpiScale.PixelsPerInchX,
+                dpiScale.PixelsPerInchY,
+                PixelFormats.Pbgra32);
+            
+            rtb.Render(element);
+            
+            return rtb;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
