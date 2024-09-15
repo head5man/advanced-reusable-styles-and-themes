676c5c2b82f9b528a3cf2b08aa128954b0363e42
Tuomas <tuomas123lahtinen@gmail.com>
2024-09-15 17:21:40 +0300

    Add ButtonProperties attached properties class with corner radius

---
+++ Globomantics.UI/Globomantics.UI.WPF/AttachedProperties/ButtonProperties.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace Globomantics.UI.WPF.AttachedProperties
+{
+    public static class ButtonProperties
+    {
+        #region CornerRadius (Attached Property)
+        /// <summary>
+        /// Defines and registers CornerRadius property.
+        /// </summary>
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.RegisterAttached(
+                "CornerRadius",
+                typeof(double),
+                typeof(ButtonProperties),
+                new PropertyMetadata(0d));
+
+        /// <summary>
+        /// Get the corner radius.
+        /// </summary>
+        public static double GetCornerRadius(DependencyObject obj)
+        {
+            return (double)obj.GetValue(CornerRadiusProperty);
+        }
+
+        /// <summary>
+        /// Set the corner radius.
+        /// </summary>
+        public static void SetCornerRadius(DependencyObject obj, double value)
+        {
+            obj.SetValue(CornerRadiusProperty, value);
+        }
+
+        #endregion
+    }
+}
--- Globomantics.UI/Globomantics.UI.WPF/Properties/AssemblyInfo.cs
+++ Globomantics.UI/Globomantics.UI.WPF/Properties/AssemblyInfo.cs
@@ -20,6 +20,7 @@ using System.Windows.Markup;
 )]
 
 [assembly: XmlnsDefinition("http://globomantics.com/2019/ui/wpf", "Globomantics.UI.WPF")]
+[assembly: XmlnsDefinition("http://globomantics.com/2019/ui/wpf", "Globomantics.UI.WPF.AttachedProperties")]
 [assembly: XmlnsDefinition("http://globomantics.com/2019/ui/wpf", "Globomantics.UI.WPF.Controls")]
 [assembly: XmlnsDefinition("http://globomantics.com/2019/ui/wpf", "Globomantics.UI.WPF.Converters")]
 
